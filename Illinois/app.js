b597fb493629ee6e1381ac907e787f0b15c1714e
jsalicetti <jessicasalicetti@yahoo.com>
2024-05-09 10:01:19 -0400

    statehw

---
+++ Illinois/app.js
@@ -0,0 +1,6 @@
+const ourName = "Jessie";
+const visited = false;
+const moneySpent = 0
+
+
+console.log(`It is a ${visited} statement that ${ourName} visited Illinois and spent ${moneySpent} dollars!`)
---
+++ Montana/app.js
@@ -0,0 +1,6 @@
+const ourName = "Jessie";
+const visited = false;
+const moneySpent = 0
+
+
+console.log(`It is a ${visited} statement that ${ourName} visited Montana and spent ${moneySpent} dollars!`)
